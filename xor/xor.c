2ff31fe8acf348ca7321e5668d2dc754f7eae006
senthil <senthil@inspiron-3000>
2015-10-19 23:42:50 +0530

    Improved xor

--- xor/xor.c
+++ xor/xor.c
@@ -11,45 +11,24 @@ int main(void)
 
     for (i = 0; i < T; i++) {
         int N;
-        unsigned int * array;
+        unsigned int num;
         int j;
 
         scanf("%d", &N);
 
-        array = malloc(N * sizeof (*array));
-
-        for (j = 0; j < N; j++)
-            scanf("%u", array + j);
-
         out[i] = 0;
 
         for (j = 0; j < N; j++) {
-            int k;
+            scanf("%u", &num);
+	    if ( !(j % 2) ) {
+		out[i] ^= num;
+	    }
+	}
 
-            unsigned int xor_sum = 0;
-            //printf("Continuous sub arrays of size: %d\n", j + 1);
-            for (k = 0; k < N - j; k++) {
-                int l;
-                int m;
-                unsigned int sub_array_xor = 0;
+	if ( !(N % 2) ) {
+	    out[i] = 0;
+	}
 
-                for (l = k, m = 0; m <= j; l++, m++) {
-                    //printf("%u ", array[l]);
-                    sub_array_xor ^= array[l];
-                }
-
-                //printf("  ");
-
-                xor_sum ^= sub_array_xor;
-            }
-
-            //printf("\n");
-
-            out[i] ^= xor_sum;
-
-        }
-
-        free(array);
     }
 
     for (i = 0; i < T; i++) {
